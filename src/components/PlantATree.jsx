4b7e41bc57f8247e47a0ade59752a14981551e6f
Akash Kumar <meakash7902@gmail.com>
2022-12-26 22:38:18 +0530

    added a function to get aqi data based on user location

--- src/components/PlantATree.jsx
+++ src/components/PlantATree.jsx
@@ -1,10 +1,58 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Card, CardHeader, CardBody, CardFooter } from '@chakra-ui/react'
 import { Button, Image, Stack, Heading, Text } from '@chakra-ui/react'
 import plantTree from '../assets/plantTree.svg'
 import { Link } from 'react-router-dom'
 
 const PlantATree = () => {
+    const [address, setAddress] = useState({ lat: null, long: null });
+    const [aqi, setAqi] = useState(null);
+
+    useEffect(() => {
+        getLocation();
+    }, []);
+
+    useEffect(() => {
+        getAQI();
+    }, [address]);
+
+    let token = process.env.REACT_APP_AQI_TOKEN;
+
+    const getLocationThroughIp = () => {
+        const url = `https://api.ipgeolocation.io/ipgeo?apiKey=${process.env.REACT_APP_IPGEO_TOKEN}`
+        fetch(url)
+            .then((res) => res.json())
+            .then((res) => {
+                setAddress({ lat: res.latitude, long: res.longitude });
+            })
+            .catch((err) => console.log(err));
+    }
+
+    function getLocation() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(showPosition, altWay);
+        }
+        else {
+            getLocationThroughIp();
+        }
+    }
+
+    async function showPosition(position) {
+        setAddress({ lat: position.coords.latitude, long: position.coords.longitude })
+    }
+
+    function altWay() {
+        getLocationThroughIp();
+    }
+
+    const getAQI = () => {
+        const url = `https://api.waqi.info/feed/geo:${address.lat};${address.long}/?token=${token}`;
+        fetch(url)
+            .then((res) => res.json())
+            .then((data) => console.log(data))
+            .catch((err) => console.log(err));
+    };
+
     return (
         <>
             <div className='flex flex-col md:flex-row mx-12 text-4xl gap-32 items-center'>
