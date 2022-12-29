4478c0e500e3328418a3477e2ae28c66425ab879
Akash Kumar <meakash7902@gmail.com>
2022-12-29 17:41:40 +0530

    made half profile page

--- src/containers/Profile.jsx
+++ src/containers/Profile.jsx
@@ -1,34 +1,31 @@
 import React from 'react'
 import { client } from '../client'
 import { useState, useEffect } from 'react'
+import logo from '../assets/Myproject.png'
 
 const Profile = () => {
   const user = JSON.parse(localStorage.getItem('user'))
-  console.log(user.name)
-  console.log(user.email)
 
   return (
-    
-    <>
-      <h1>Profile</h1>
-      <div>
-        <div className='py-10 text-7xl font-extrabold md:order-1 order-2 text-center text-transparent bg-clip-text bg-gradient-to-r from-green-600 to-red-600'> 
-            User Profile
-        </div>
-        <div> 
-            <div className="flex flex-row items-center justify-center gap-2 w-full md:w-1/2 ">
-                <img
-                  src={user?.picture}
-                  alt="user-profile"
-                  className="w-50 h-50 rounded-full"
-                />
-                <h3 className="font-bold text-3xl">{user.name}</h3>
-                <h4 className='text-2xl'> Email: {user.email}</h4>
-            </div>
-        </div>
-
+    <div className='flex flex-col w-full items-center'>
+      <div className="h-370 2xl:h-510 relative w-4/5 user-background mb-20">
+        <img
+          className='absolute -bottom-10 right-1/2 translate-x-[50%] rounded-full w-20 h-20 shadow-xl object-cover'
+          src={user?.picture}
+          alt="user-pic"
+        />
       </div>
-    </>
+      <div className="flex justify-between w-4/5 border-2 p-3 rounded-lg drop-shadow-2xl">
+        <p className="font-semibold text-xl flex flex-row items-center gap-5">
+          <img src={user?.picture} alt="" className="rounded-lg w-[25px]" />
+          <p> {user?.name}</p>
+        </p>
+        <p className='text-gray-500'>
+          Joined on
+        </p>
+      </div>
+
+    </div>
   )
 }
 
