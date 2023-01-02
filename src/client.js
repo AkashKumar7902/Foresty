96ef9b789e39c4e20d78dae60b6389c8b9dc67ff
Akash Kumar <meakash7902@gmail.com>
2023-01-02 18:02:05 +0530

    attempt to shift to new dataset

--- src/client.js
+++ src/client.js
@@ -3,7 +3,7 @@ import imageUrlBuilder from '@sanity/image-url';
 
 export const client = sanityClient({
     projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
-    dataset: 'production',
+    dataset: 'foresty',
     apiVersion: '2022-11-16',
     useCdn: true,
     token: process.env.REACT_APP_SANITY_TOKEN,
